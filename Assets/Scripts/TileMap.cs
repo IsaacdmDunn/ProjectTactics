a3f62aa5776075cee16ecedc95c4ffd8ae5809d1
Isaac Dunn <57407700+IsaacdmDunn@users.noreply.github.com>
2020-06-19 17:36:24 +0100

    Added tile generation
    A tile map is generated with 3 different tile types (grass, swamp and mountains).

---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour {
+
+	public TileType[] tileTypes;
+	int[,] tiles;
+
+	int mapSizeY = 10;
+	int mapSizeX = 10;
+
+	// Use this for initialization
+	void Start () {
+		//allocate map tiles
+		tiles = new int[mapSizeX, mapSizeY];
+
+		//init map tiles
+		for (int x = 0; x < mapSizeX; x++) {
+			for (int y = 0; y < mapSizeY; y++) {
+				tiles [x, y] = 0;
+			}
+		}
+
+		//mountain
+		tiles [4, 4] = 2;
+		tiles [4, 4] = 2;
+		tiles [4, 4] = 2;
+		tiles [4, 4] = 2;
+		tiles [4, 4] = 2;
+		tiles [4, 5] = 2;
+		tiles [4, 6] = 2;
+		tiles [8, 5] = 2;
+		tiles [8, 6] = 2;
+
+		GenerateMapVisuals();
+	}
+
+	//spawn prefabs
+	void GenerateMapVisuals() {
+		for (int x = 0; x < mapSizeX; x++) {
+			for (int y = 0; y < mapSizeY; y++) {
+
+				TileType tt = tileTypes[tiles[x,y]];
+				Instantiate (tt.tileVisualPrefab, new Vector3(x, y, 0), Quaternion.identity);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
