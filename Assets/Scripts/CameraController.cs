0aa7554d6933e6a3a13d48ddab32d65b08fe717d
Isaac Dunn <57407700+IsaacdmDunn@users.noreply.github.com>
2020-06-21 00:24:41 +0100

    V0.20 new cameras
    Added an extra camera for top-down view and an option to switch between the new camera and the isometric camera using "V"

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Camera cam1;
+    public Camera cam2;
+ 
+    void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.V) && (cam1.enabled == true || cam2.enabled == false))
+        {
+            cam1.enabled = false;
+            cam2.enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.V) && (cam2.enabled == true || cam1.enabled == false))
+        {
+            cam2.enabled = false;
+            cam1.enabled = true;
+        }
+    }
+}
