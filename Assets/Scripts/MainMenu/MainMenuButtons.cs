c840739128f307ec447b3826dcf3bbaa0c95f667
Isaac Dunn <57407700+IsaacdmDunn@users.noreply.github.com>
2020-06-22 01:46:50 +0100

    V0.32 Main menu functionality
    Added ability to switch between main menu scene and the game scene using the play button and quit the game using the quit button

---
+++ Assets/Scripts/MainMenu/MainMenuButtons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+
+    //represents the button objects in the main menu
+    public GameObject PlayButton;
+    public GameObject HelpButton;
+    public GameObject QuitButton;
+
+    //plays the game scene when the play button is pressed
+    public void PlayButtonPressed()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    //quits the game when the quit button is pressed
+    public void QuitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+    //displays the help menu when the help button is pressed
+    public void HelpButtonPressed()
+    {
+
+    }
+}
